9c3df419dc03c5ccc9608555553aacf7222e994d
ys <yunsheng.zhao@bydeluxe.com>
2021-12-16 22:47:41 -0800

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,27 +1,30 @@
 #!/bin/bash                                                                                                                                                                                                                            
-declare -i _sum=0
+
 get_total(){
 	total=$(( total + 1 ))
 	echo $total
 }
+
 rand_str(){ 
 	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1 
 }
-prt_str(){
-	let _sum++
 
+prt_str(){
 	ts=$1
 	idx=$2
 	str=$(rand_str)
-	echo "logID2=$ts, log[$idx/$_sum] $str"
+	echo "group=$ts, item=$idx, $str"
 }
+
 main(){
 	ts=$(date +%s)
 	num=$LOG_ITEMS
-	echo "testing log $num items"
+	echo "log $num items start"
 	for ((i=0;i<$num;i++)); do 
 		prt_str $ts $i
 	done
+	echo "log $num items stop"
 	sleep infinity
 }
+
 main
